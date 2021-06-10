f632790ceb18b2aa935cc80defe6e6d6a24a7a6f
prenassipaolo <prenassipaolo@gmail.com>
2021-06-10 19:39:12 +0200

    add loading data function

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,5 +1,25 @@
 import torch
 from torch import nn
-from torchvision import DataLoader
+from torch.utils.data import DataLoader
 from torchvision  import datasets
 from torchvision.transforms import ToTensor
+
+def download_mnist_datasets():
+    train_data = datasets.MNIST(
+        root="data",
+        download=True,
+        train=True,
+        transform=ToTensor()
+    )
+    validation_data = datasets.MNIST(
+        root="data",
+        download=True,
+        train=False,
+        transform=ToTensor()
+    )
+    return train_data, validation_data
+
+if __name__=="__main__":
+    # download MNIST dataset
+    train_data, _ = download_mnist_datasets()
+    print("MNIST dataset downloaded")
