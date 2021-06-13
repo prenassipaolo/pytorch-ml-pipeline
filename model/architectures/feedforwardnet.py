10872fc945ca049f7f8e17e2ce9d8b6a576fed9c
prenassipaolo <prenassipaolo@gmail.com>
2021-06-13 12:30:59 +0200

    make the feed forward network more customizable

--- model/architectures/feedforwardnet.py
+++ model/architectures/feedforwardnet.py
@@ -3,24 +3,22 @@ from torch import nn
 
 class FeedForwardNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, parameters):
         super().__init__()
+
+        self.in_dim = parameters['in_dim']
+        self.hidden_dim = parameters['hidden_dim']
+        self.out_dim = parameters['out_dim']
         self.flatten = nn.Flatten()
         self.dense_layers = nn.Sequential(
-            nn.Linear(28 * 28, 256),
+            nn.Linear(self.in_dim, self.hidden_dim),
             nn.ReLU(),
-            nn.Linear(256, 10)
+            nn.Linear(self.hidden_dim, self.out_dim)
         )
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, x):
-        x = self.flatten(x)
+    def forward(self, input_data):
+        x = self.flatten(input_data)
         x = self.dense_layers(x)
         x = self.softmax(x)
         return x
-
-
-
-
-
-
