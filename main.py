0a5e3ccd7007c18310b0fbfae2181dae9083e729
prenassipaolo <prenassipaolo@gmail.com>
2021-06-19 10:51:00 +0200

    create main

---
+++ main.py
@@ -0,0 +1,9 @@
+from model.model import Model
+from data.download_data import download_mnist_datasets
+
+parameters_path = "model/default_parameters.json"
+train_set, test_set = download_mnist_datasets()
+
+M = Model(parameters_path=parameters_path)
+M.do_train(train_set, test_set)
+M.save(inplace=True)
